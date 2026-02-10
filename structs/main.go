63c9e8e254084c68e10329a9b7004d58eb365262
Saswata Mondal <121812665+msaswata15@users.noreply.github.com>
2026-02-10 09:35:56 +0530

    add contactinfo struct and update person initialization in main function

--- structs/main.go
+++ structs/main.go
@@ -5,6 +5,11 @@ import "fmt"
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactinfo
+}
+type contactinfo struct {
+	email   string
+	zipcode int
 }
 
 func main() {
@@ -15,6 +20,32 @@ func main() {
 	var allen person
 	var rmp person
 	fmt.Printf("%+v", rmp)
-	allen = person{"Allen", "Das"}
-	fmt.Println(allen)
+	fmt.Println()
+	allen = person{"Allen", "Das", contactinfo{"msaswata@gmail.com", 721607}}
+	fmt.Println(allen.firstName)
+	fmt.Println(allen.lastName)
+	fmt.Println(allen.contact.email)
+	fmt.Println(allen.contact.zipcode)
+	allen.print()
+	alex.print()
+	fmt.Println()
+	fmt.Println()
+
+	allen.updateName("Sam").print()
+	fmt.Println()
+	fmt.Println()
+	allen.updateNamebyreference("Sonu")
+	allen.print()
+
+}
+func (p person) updateName(newFirstName string) person {
+	p.firstName = newFirstName
+	return p
+}
+func (p *person) updateNamebyreference(newFirstName string) {
+	p.firstName = newFirstName
+
+}
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
